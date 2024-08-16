889794a9101ebbde4c2564dec7334a433f29e7f9
YuriK(MAC) <yuriy.ksh@thewhitelabelagency.com>
2024-08-16 14:23:58 +0300

    Added style for firstBlock + added @wordpress/create-block = in folder boilerplate

--- wp-content/plugins/firstblock/src/index.js
+++ wp-content/plugins/firstblock/src/index.js
@@ -1,11 +1,9 @@
-// var registerBlockType = wp.blocks.registerBlockType
 import { registerBlockType } from '@wordpress/blocks'
+import Edit from './edit'
+import Save from './save'
+
 
 registerBlockType('blocks-course/firstblock', {
-  edit: function () {
-    return <p className="src-test-class">Edit 123</p>
-  },
-  save: function () {
-    return <p className="src-test-class">Save</p>
-  },
+  edit: Edit,
+  save: Save,
 })
