ed7fc350c0bc788f81a939b9bd93b878a9498679
Yuriy K <yuriy.ksh@thewhitelabelagency.com>
2024-09-17 11:25:16 +0300

    Add toolbar for block-editor in admin panel

--- wp-content/plugins/text-box/src/edit.js
+++ wp-content/plugins/text-box/src/edit.js
@@ -1,19 +1,121 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+} from '@wordpress/block-editor';
+import { useState } from '@wordpress/element';
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	DropdownMenu,
+	ToolbarDropdownMenu,
+} from '@wordpress/components';
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
+	console.log(attributes);
 	const { text } = attributes;
+	const [activeButton, setActiveButton] = useState(null);
 	return (
-		<RichText
-			{...useBlockProps()}
-			onChange={(val) => {
-				setAttributes({ text: val });
-			}}
-			value={text}
-			placeholder={__('Your Text', 'text-box')}
-			tagName="h4"
-			allowedFormats={['core/bold']}
-		/>
+		<>
+			<BlockControls group="inline">
+				<p>Inline Controls</p>
+			</BlockControls>
+			<BlockControls group="block">
+				<p>Block Controls</p>
+			</BlockControls>
+			{text && (
+				<BlockControls
+					group="other"
+					controls={[
+						{
+							title: 'Button 1',
+							icon: 'admin-generic',
+							isActive: activeButton === 'button1',
+							onClick: () => {
+								setActiveButton(
+									activeButton === 'button1'
+										? null
+										: 'button1'
+								);
+							},
+						},
+						{
+							title: 'Button 2',
+							icon: 'admin-collapse',
+							isActive: activeButton === 'button2',
+							onClick: () => {
+								setActiveButton(
+									activeButton === 'button2'
+										? null
+										: 'button2'
+								);
+							},
+						},
+					]}
+				>
+					<p>Other</p>
+					<ToolbarGroup>
+						<ToolbarButton
+							title="Align Left"
+							icon="editor-alignleft"
+							onClick={() => console.log('Align left')}
+						/>
+						<ToolbarButton
+							title="Align Center"
+							icon="editor-aligncenter"
+							onClick={() => console.log('Align Center')}
+						/>
+						<ToolbarButton
+							title="Align Right"
+							icon="editor-alignright"
+							onClick={() => console.log('Align Right')}
+						/>
+						<DropdownMenu
+							icon="arrow-down-alt2"
+							label={__('More AlignMenTS', 'text-box')}
+							controls={[
+								{
+									title: __('Wide', 'text-box'),
+									icon: 'align-wide',
+								},
+								{
+									title: __('Full', 'text-box'),
+									icon: 'align-full-width',
+								},
+							]}
+						/>
+						<ToolbarDropdownMenu
+							icon="arrow-down-alt2"
+							label={__('More AlignMenTS', 'text-box')}
+							controls={[
+								{
+									title: __('Wide', 'text-box'),
+									icon: 'align-wide',
+								},
+								{
+									title: __('Full', 'text-box'),
+									icon: 'align-full-width',
+								},
+							]}
+						/>
+					</ToolbarGroup>
+					<ToolbarGroup>
+						<p>Next Icon Group</p>
+					</ToolbarGroup>
+				</BlockControls>
+			)}
+			<RichText
+				{...useBlockProps()}
+				onChange={(val) => {
+					setAttributes({ text: val });
+				}}
+				value={text}
+				placeholder={__('Your Text', 'text-box')}
+				tagName="h4"
+				allowedFormats={[]}
+			/>
+		</>
 	);
 }
