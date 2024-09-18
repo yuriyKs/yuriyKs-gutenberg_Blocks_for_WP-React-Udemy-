27f1a8209d3e314e78a479fa0c9dd289737e9425
Yuriy K <yuriy.ksh@thewhitelabelagency.com>
2024-09-18 11:00:38 +0300

    AlignmentToolbar Component gutenberg+live_site

--- wp-content/plugins/text-box/src/edit.js
+++ wp-content/plugins/text-box/src/edit.js
@@ -3,6 +3,7 @@ import {
 	useBlockProps,
 	RichText,
 	BlockControls,
+	AlignmentToolbar,
 } from '@wordpress/block-editor';
 import { useState } from '@wordpress/element';
 import {
@@ -14,103 +15,26 @@ import {
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
-	console.log(attributes);
-	const { text } = attributes;
-	const [activeButton, setActiveButton] = useState(null);
+	const { text, alignment } = attributes;
+	const onChangeText = (nextText) => {
+		setAttributes({ text: nextText });
+	};
+	const onChangeAlignment = (nextAlignment) => {
+		setAttributes({ alignment: nextAlignment });
+	};
 	return (
 		<>
 			<BlockControls group="inline">
-				<p>Inline Controls</p>
+				<AlignmentToolbar
+					value={alignment}
+					onChange={(val) => onChangeAlignment(val)}
+				/>
 			</BlockControls>
-			<BlockControls group="block">
-				<p>Block Controls</p>
-			</BlockControls>
-			{text && (
-				<BlockControls
-					group="other"
-					controls={[
-						{
-							title: 'Button 1',
-							icon: 'admin-generic',
-							isActive: activeButton === 'button1',
-							onClick: () => {
-								setActiveButton(
-									activeButton === 'button1'
-										? null
-										: 'button1'
-								);
-							},
-						},
-						{
-							title: 'Button 2',
-							icon: 'admin-collapse',
-							isActive: activeButton === 'button2',
-							onClick: () => {
-								setActiveButton(
-									activeButton === 'button2'
-										? null
-										: 'button2'
-								);
-							},
-						},
-					]}
-				>
-					<p>Other</p>
-					<ToolbarGroup>
-						<ToolbarButton
-							title="Align Left"
-							icon="editor-alignleft"
-							onClick={() => console.log('Align left')}
-						/>
-						<ToolbarButton
-							title="Align Center"
-							icon="editor-aligncenter"
-							onClick={() => console.log('Align Center')}
-						/>
-						<ToolbarButton
-							title="Align Right"
-							icon="editor-alignright"
-							onClick={() => console.log('Align Right')}
-						/>
-						<DropdownMenu
-							icon="arrow-down-alt2"
-							label={__('More AlignMenTS', 'text-box')}
-							controls={[
-								{
-									title: __('Wide', 'text-box'),
-									icon: 'align-wide',
-								},
-								{
-									title: __('Full', 'text-box'),
-									icon: 'align-full-width',
-								},
-							]}
-						/>
-						<ToolbarDropdownMenu
-							icon="arrow-down-alt2"
-							label={__('More AlignMenTS', 'text-box')}
-							controls={[
-								{
-									title: __('Wide', 'text-box'),
-									icon: 'align-wide',
-								},
-								{
-									title: __('Full', 'text-box'),
-									icon: 'align-full-width',
-								},
-							]}
-						/>
-					</ToolbarGroup>
-					<ToolbarGroup>
-						<p>Next Icon Group</p>
-					</ToolbarGroup>
-				</BlockControls>
-			)}
 			<RichText
-				{...useBlockProps()}
-				onChange={(val) => {
-					setAttributes({ text: val });
-				}}
+				{...useBlockProps({
+					className: `text-alignment-${alignment}`,
+				})}
+				onChange={onChangeText}
 				value={text}
 				placeholder={__('Your Text', 'text-box')}
 				tagName="h4"
--- wp-content/plugins/text-box/src/save.js
+++ wp-content/plugins/text-box/src/save.js
@@ -1,15 +1,15 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
-export default function save({ attributes, setAttributes }) {
-	const { text } = attributes;
+export default function save({ attributes }) {
+	const { text, alignment } = attributes;
+	console.log('alignment', alignment);
 	return (
 		<RichText.Content
-			{...useBlockProps.save()}
+			{...useBlockProps.save({
+				className: `text-alignment-${alignment}`,
+			})}
 			tagName="h4"
-			onChange={(val) => {
-				setAttributes({ text: val });
-			}}
 			value={text}
 		/>
 	);
